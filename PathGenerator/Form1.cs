e315273b0fa68c0b9fd279039725bc9b87d57f4d
Klakier <lukasz.paleczek@gmail.com>
2019-11-18 15:53:46 +0100

    EOD

--- PathGenerator/Form1.cs
+++ PathGenerator/Form1.cs
@@ -23,17 +23,17 @@ namespace PathGenerator
         public Point initLocButton3;
         public Point initLocTextBox2;
 
-        private const double Z_VALUE = -7;
-        private const double X_SIZE = 340;
-        private const double Y_SIZE = 133;
-        private const double X_OFFSET = 24; //po obu stronach, czarne zaczyna sie od 12mm
-        private const double X_START = 0; //tylko na poczatku
-        private const int LINES = 12;
-        private readonly double[] Y_POINTS = new double[] { 19.1, 27, 57, 67, 77, 107, 114.9 };
-        private readonly double[] SPEED = new double[] { 0.5, 0.25, 0.20, 0.1, 0.1, 0.2, 0.25 };
-        private readonly double[] Y_POINTS_REV = new double[] { 19.1, 27, 57, 67, 77, 107, 114.9 }.Reverse().ToArray();
-        private readonly double[] SPEED_REV = new double[] { 0.5, 0.25, 0.20, 0.1, 0.1, 0.2, 0.25 };
-        private const int STOP_GLUE_POINT = 5;
+        private const double Z_VALUE = -10;
+        private const double Y_SIZE = 340;
+        private const double X_SIZE = 133;
+        private const double Y_OFFSET = 24; //po obu stronach, czarne zaczyna sie od 12mm
+        private const double Y_START = 0; //tylko na poczatku
+        private const int LINES = 26;
+        private readonly double[] X_POINTS = new double[] { 6, 16, 50, 83, 117, 127 };
+        private readonly double[] SPEED = new double[] { 0.5, 0.3, 0.25, 0.15, 0.25, 0.3 };
+        private readonly double[] X_POINTS_REV = new double[] { 6, 16, 50, 83, 117, 127 }.Reverse().ToArray();
+        private readonly double[] SPEED_REV = new double[] { 0.5, 0.3, 0.25, 0.15, 0.25, 0.3 };
+        private const int STOP_GLUE_POINT = 4;
         private const int START_GLUE_POINT = 1;
 
         private const int TOOL = 1;
@@ -64,11 +64,11 @@ namespace PathGenerator
             StringBuilder stringBuilderDAT = new StringBuilder();
 
             //generowanie punktów w X            
-            double[] x_points = new double[LINES];
-            double x_spracing = (X_SIZE - 2.0 * X_OFFSET - X_START) / (LINES - 1);
+            double[] y_points = new double[LINES];
+            double y_spracing = (Y_SIZE - 2.0 * Y_OFFSET - Y_START) / (LINES - 1);
             for (int i = 0; i < LINES; i++)
             {
-                x_points[i] = X_OFFSET + X_START + x_spracing * i;
+                y_points[i] = Y_OFFSET + Y_START + y_spracing * i;
             }
 
             List<E6POS> points = new List<E6POS>();
@@ -78,14 +78,14 @@ namespace PathGenerator
             for (int i = 0; i < LINES; i++)
             {
                 bool evenOrOdd = (i % 2 == 0);
-                double[] y_points = evenOrOdd ? Y_POINTS : Y_POINTS_REV; //dla parzysty punkty normalne a nie parzystych odwrotne
+                double[] x_points = evenOrOdd ? X_POINTS : X_POINTS_REV; //dla parzysty punkty normalne a nie parzystych odwrotne
                 double[] speed = evenOrOdd ? SPEED : SPEED_REV;
 
-                for (int j = 0; j < Y_POINTS.Count(); j++)
+                for (int j = 0; j < X_POINTS.Count(); j++)
                 {
                     string nameE6POS = String.Format("XKLB_{0}_{1}", (i + 1).ToString("D2"), (j + 1).ToString("D2"));
                     string nameFDAT = String.Format("FKLB_{0}_{1}", (i + 1).ToString("D2"), (j + 1).ToString("D2"));
-                    E6POS e6pos = new E6POS(x_points[i], y_points[j], Z_VALUE, nameE6POS);
+                    E6POS e6pos = new E6POS(x_points[j], y_points[i], Z_VALUE, nameE6POS);
                     FDAT fdat = new FDAT(TOOL, BASE, nameFDAT);
                     SLIN slin;
                     switch (j)
